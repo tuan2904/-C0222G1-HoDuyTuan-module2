7605c4685970ed3a1acc5fd4391a579d8bba064e
tuan2904 <dtuan290400@gmail.com>
2022-04-02 19:28:36 +0700

    i

--- Module_2/src/_03_array/exercise/DellElement.java
+++ Module_2/src/_03_array/exercise/DellElement.java
@@ -11,6 +11,7 @@ public class DellElement {
         int elements = enters.nextInt();
         int[] array = new int[elements];
         int count=0;
+        int count1=0;
         System.out.println("Enter elements ");
         for (int i = 0; i < array.length; i++) {
             System.out.println(" Enter elements");
@@ -27,33 +28,65 @@ public class DellElement {
             System.out.println("position of x "  + count);
         System.out.println(" Enter the location to insert");
         x = enters.nextInt();
-        int i;
-        for (i=0;i<array.length;i++) {
-            if (array[i] != x) {
-                return;
-            }else if (array[i] == x) {
-                       array[i]=array.length-1;
-
+            for (int  i = 0; i < array.length ;) {
+                if (array[i] == x){
+                    count1 = i;
+                    break;
+                }else {
+                    i++;
+                }
             }
-
-        }System.out.println(array[i]);
+            for (int j = count1; j < array.length ; j++) {
+                if (j == array.length - 1){
+                    array[j] = 0;
+                }else {
+                    array[j] =array[j + 1];
+                }
+            }
+            System.out.println(Arrays.toString(array));
+        }
+//        int i;
+//        for (i=0;i<array.length;i++) {
+//            if ( x==array[i]){
+//                count1=i;
+//            }}
+//            System.out.println(count1);
+//            for (int j=count1;i<array.length-1;j++){
+////                    array[j]=array[j-1];
+//                if (j==array.length-1){
+//                    array[j]=0;
+//                }else {
+//                    array[j]=array[j+1];
+//                }}
+////            }array[array.length-1]=0;
 //        System.out.println(Arrays.toString(array));
-
-
-
-
+////            if (array[i] != x) {
+////                return;
+////            }else if (array[i] == x) {
+////                       array[array.length-1]=0;
+////                System.out.println(array[i]);
+////            }
+////
 //
-//            System.out.println(Arrays.toString(array1));
-//        List<int>list = new ArrayList<Integer>();
-//                for (int i=0;i<array.length-1;i++){
-//                    if (array[i] != x){
-//                        list.add(array[i]);
-//                    }
-//                }
-//                array=list.toArray(new int[0]);
-//                }
+//        System.out.println(Arrays.toString(array));
+////        System.out.println(Arrays.toString(array));
+//
+//
+//
+//
+////
+////            System.out.println(Arrays.toString(array1));
+////        List<int>list = new ArrayList<Integer>();
+////                for (int i=0;i<array.length-1;i++){
+////                    if (array[i] != x){
+////                        list.add(array[i]);
+////                    }
+////                }
+////                array=list.toArray(new int[0]);
+////                }
+//
 
 
 
+}
 
-}}
