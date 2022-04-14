53cd30f6f47140dc493dc80560755e4136a0e18c
tuan2904 <dtuan290400@gmail.com>
2022-04-14 21:47:41 +0700

    l

--- Module_2/src/_10_stack_queue/exercise/Bracket.java
+++ Module_2/src/_10_stack_queue/exercise/Bracket.java
@@ -8,26 +8,29 @@ import java.util.Stack;
 public class Bracket {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str=scanner.nextLine();
+        System.out.println("Input string: ");
+        String str = scanner.nextLine();
         Stack<String> stack = new Stack<>();
-        String []strings=str.split("");
+        String[] strings = str.split("");
 
         String flag;
         String string;
-        for (int i=0;i<strings.length;i++){
+        for (int i = 0; i < strings.length; i++) {
 
-            if (strings[i].equals("(") ){
+            if (strings[i].equals("(")) {
                 stack.push(strings[i]);
-            } if (strings[i].equals(")")){
-                if (stack.isEmpty()){
-                    flag="false";
-                }string=stack.pop();
-                if (string.equals(strings[i])){
-                    flag="false";
+            }
+            if (strings[i].equals(")")) {
+                if (stack.isEmpty()) {
+                    flag = "false";
+                }
+                string = stack.pop();
+                if (string.equals(strings[i])) {
+                    flag = "false";
                 }
             }
         }
-        if (stack.isEmpty()){
+        if (stack.isEmpty()) {
             System.out.println("True");
         } else {
             System.out.println("false");
