1acc98a68c3b81fa1abfd59fa7998ddf129c6fee
tuan2904 <dtuan290400@gmail.com>
2022-04-14 21:15:29 +0700

    l

--- Module_2/src/_10_stack_queue/exercise/Palindrome.java
+++ Module_2/src/_10_stack_queue/exercise/Palindrome.java
@@ -2,27 +2,31 @@ package _10_stack_queue.exercise;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class Palindrome {
     public static void main(String[] args) {
-
         Queue<String> queue = new LinkedList<>();
-        Stack<String>stack= new Stack<>();
-        Scanner input = new Scanner(System.in);
-        String str = input.nextLine();
+        Stack<String> stack = new Stack<>();
+        String str = "Able was I ere I saw Elba";
         String[] array = str.toLowerCase().split("");
         for (int i = 0; i < array.length; i++) {
             queue.add(array[i]);
             stack.push(array[i]);
         }
+        System.out.println("show array stack: ");
         System.out.println(stack);
+        System.out.println("show array queue: ");
         System.out.println(queue);
-        for (int i = 0; i <array.length ; i++) {
-            if (!stack.pop().equals(queue.poll())){
-                
-    }
-}
+        boolean flag = true;
+        for (int i = 0; i < array.length; i++) {
+            if (!stack.pop().equals(queue.poll())) {
+                flag = false;
+                break;
+            }
+        }
+        System.out.println("Đây có phải mảng Palindrome không: ");
+        System.out.println(flag);
+
     }
 }
