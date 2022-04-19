ab4aca055131d7ca71308e662ba8ab69cfd31e7a
tuan2904 <dtuan290400@gmail.com>
2022-04-20 06:47:06 +0700

    l

---
+++ Module_2/src/_15_debug_exceptionhandling/practice/ArrayExample.java
@@ -0,0 +1,29 @@
+package _15_debug_exceptionhandling.practice;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayExample {
+    public Integer[] createRandom() {
+        Random rd = new Random();
+        Integer[] arr = new Integer[100];
+        System.out.println("Danh sách phần tử trong mảng: ");
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        Integer[] arr = arrayExample.createRandom();
+        Scanner enter = new Scanner(System.in);
+        System.out.println("Vui lòng nhập chỉ số của một phần tử bất kỳ: ");
+        int x = enter.nextInt();
+        try {
+            System.out.println("Giá trị có chỉ số 5: " + x + " là " + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Chỉ số vượt quá giới hạn của mảng: ");
+        }
+    }
+}
---
+++ Module_2/src/casestudy/models/Villa.java
@@ -0,0 +1,5 @@
+package casestudy.models;
+
+public class Villa {
+
+}
